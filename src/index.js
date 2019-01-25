e08f09381802b93eb1388bd70905a8c268f625b0
thomasgit123 <thomasbox123@gmail.com>
2019-01-24 23:03:14 -0500

    integrate redux sagar & redux logger

--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from "redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+
+import { reducer } from "./store/redux";
+import { watcherSaga } from "./store/sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+middlewares.push(logger);
+
+const store = compose(applyMiddleware(...middlewares))(createStore)(reducer);
+
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
